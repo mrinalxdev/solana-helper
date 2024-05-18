7df405a9994297f90438e792eea6858b540811cc
Mrinal Pramanick <mrinalbitsat@gmail.com>
2024-05-18 14:38:56 +0530

    Added New Data

--- src/bot.js
+++ src/bot.js
@@ -1,4 +1,4 @@
-const {Client, GatewayIntentBits} = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 require("dotenv").config();
 
 const TOKEN = process.env.BOT_TOKEN;
@@ -6,15 +6,14 @@ const clientId = process.env.CLIENT_ID;
 const guildId = process.env.GUILD_ID;
 
 const client = new Client({
-  intents : [
+  intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildVoiceStats,
+    GatewayIntentBits.GuildVoiceStates,
   ],
-  
 });
 
 (async () => {
   await client.login(TOKEN);
-  console.log(`${client.user.tag} is online!`)
+  console.log(`${client.user.tag} is online!`);
 })();
